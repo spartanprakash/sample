6f7374220ba2f9a004e4a50acf392ae406db7a02
Admin <Admin@Prakash>
2025-04-01 18:19:18 +0530

    intial commoit

---
+++ learn-selenium/src/main/java/sample/SampleTestNGTC.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTestNGTC {
+
+	@Test
+	public void runLogin() {
+		EdgeDriver driver = new EdgeDriver();
+		driver.get("http://leaftaps.com/opentaps/");
+	}
+	
+}
